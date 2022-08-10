9399fc5cd8dc212674d51454075a85489cbb44fe
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-10 20:06:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ transactionHttp
   })
 }, 500)
 
-
+// Transaction Type を返す関数
 function getTransactionType (type) { // https://symbol.github.io/symbol-sdk-typescript-javascript/1.0.3/enums/TransactionType.html
   switch(type){
   　case 16720:
