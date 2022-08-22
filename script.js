e4f432b07b4260b47bfb2824fb620e626f094a2a
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-22 16:15:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ accountHttp.getAccountInfo(address)
          // 承認音を鳴らす
         var my_audio = new Audio("https://github.com/symbol/desktop-wallet/raw/dev/src/views/resources/audio/ding2.ogg");
         my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
-        setTimeout(location.reload(),500); //リロードに0.5秒待つ
-        my_audio.play();  //サウンドを再生 
+        my_audio.play();  //サウンドを再生
+        setTimeout(location.reload(),500); //リロードに0.5秒待つ 
     });
 
     //未承認トランザクションの検知
