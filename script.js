8222e411c019fce8499b173ad28fea0fa3c665d4
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-22 18:15:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ accountHttp.getAccountInfo(address)
         //受信後の処理を記述
         console.log(tx);
          // 承認音を鳴らす
-        window.setTimeout(location.reload(),2000); // 2秒後にページをリロード
+        window.setTimeout(location.reload(),5000); // 5秒後にページをリロード
         var my_audio = new Audio("https://github.com/symbol/desktop-wallet/raw/dev/src/views/resources/audio/ding2.ogg");
         my_audio.currentTime = 0;  //再生開始位置を先頭に戻す      
         my_audio.play();  //サウンドを再生
