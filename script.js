591fdbcf6b7b9342781e7e7e14b1301d6d053c41
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-19 21:51:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -154,18 +154,14 @@ transactionHttp
            if (tx.mosaics === ""){   
               dom_amount.innerHTML = `<font color="#FF0000">🥳➡️💰 : No mosaic</font>`;     // No mosaic
               return;
-             }else{
               dom_amount.innerHTML = `<font color="#FF0000">🥳➡️💰 : ${tx.mosaics[0].amount.lower/1000000} (XYM)</font>`;     // 　数量 
-             }
            }
               
          }else {
            if (tx.mosaics === ""){   
               dom_amount.innerHTML = `<font color="#008000">💰➡️🥳 : No mosaic</font>`;     // No mosaic
               return;
-             }else{
               dom_amount.innerHTML = `<font color="#008000">💰➡️🥳 : ${tx.mosaics[0].amount.lower/1000000} (XYM)</font>`;     // 　数量 
-             }
            } 
          }
         
