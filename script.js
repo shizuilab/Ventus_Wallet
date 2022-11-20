ebf9a5b6c0efa90874556b5328ff01746ab50b59
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-20 16:49:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -152,10 +152,10 @@ transactionHttp
          
          dom_recipient_address.innerHTML = `<font color="#2f4f4f">To :   ${tx.recipientAddress.address}</font>`; //  文字列の結合　宛先
         
-      let  mosaicNames;
+      
         if (tx.mosaics.length !== 0){   //モザイクが空でない場合 Namespaceを取得する
          (async() => {
-           mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId("6BED913FA20223F8")]).toPromise();
+          const mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId("6BED913FA20223F8")]).toPromise();
           })();  
             console.log("mosaicNames=");
             console.log(mosaicNames);
