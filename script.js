78a0443b9432afb62f2ed87bcda587887c2a4966
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-20 10:58:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const transactionHttp = repositoryFactory.createTransactionRepository();
 const mosaicHttp = repositoryFactory.createMosaicRepository();
 
 console.log(mosaicHttp);
+consolo.log(mosaicHttp.getMosaicInfo("6BED913FA20223F8"));
 
 setTimeout(() => {
   
