9c9e3fc0181db928cd491104cfbfce942ba92d2e
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-20 22:34:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ transactionHttp
               dom_amount.innerHTML = `<font color="#FF0000">🥳➡️💰 : </font>`;     // 　数量 
            }else {
               dom_mosaic.innerHTML = `<font color="#FF0000">Mosaic :  ${[mosaicNames][0][0].names[0].name}  (${tx.mosaics[0].id.id.toHex()})</font>`; 
-              dom_amount.innerHTML = `<font color="#FF0000">🥳➡️💰 : ${tx.mosaics[0].amount.lower/(10*div)} </font>`;     // 　数量 
+              dom_amount.innerHTML = `<font color="#FF0000">🥳➡️💰 : ${tx.mosaics[0].amount.lower/(10^div)} </font>`;     // 　数量 
            }   
          }else {
            if (tx.mosaics.length === 0){   //モザイクが空の場合
@@ -176,7 +176,7 @@ transactionHttp
               dom_amount.innerHTML = `<font color="#008000">💰➡️🥳 : </font>`;     // 　数量 
            }else {
               dom_mosaic.innerHTML = `<font color="#008000">Mosaic :  ${[mosaicNames][0][0].names[0].name}  (${tx.mosaics[0].id.id.toHex()})</font>`; 
-              dom_amount.innerHTML = `<font color="#008000">💰➡️🥳 : ${tx.mosaics[0].amount.lower/(10*div)} </font>`;     // 　数量 
+              dom_amount.innerHTML = `<font color="#008000">💰➡️🥳 : ${tx.mosaics[0].amount.lower/(10^div)} </font>`;     // 　数量 
            }
          }
         
