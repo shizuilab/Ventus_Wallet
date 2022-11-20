d6bc584d618dccac3783d90c66a2c4771f0aa1be
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-20 20:25:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ transactionHttp
           mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId(tx.mosaics[0].id.id.toHex())]).toPromise();
           console.log(mosaicNames);        
         
-   //     }
+   //    }
          
           
       
