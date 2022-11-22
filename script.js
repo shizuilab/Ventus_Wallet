4e1060718f5f8ad4968037870b3a980493152959
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-23 00:28:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ accountHttp.getAccountInfo(address)
     for (let m of accountInfo.mosaics) {
          //console.log("m=");
        (async() => {  
-         const mosaicNames = await nsRepo.getMosaicsNames([new symbol.MosaicId(m.id.id.toHex())]).toPromise();  
-         console.log('${m.id.id.toHex()} : ${mosaicNames}');    //モザイクID と　ネームスペース
+         const mosaicNamesA = await nsRepo.getMosaicsNames([new symbol.MosaicId(m.id.id.toHex())]).toPromise();  
+         console.log('${m.id.id.toHex()} : ${[mosaicNamesA][0][0].names[0].name}');    //モザイクID と　ネームスペース
         })(); // async()    
 
       if (m.id.id.toHex() === XYM_ID) {
