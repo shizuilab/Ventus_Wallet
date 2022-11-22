bb323c2ff3368e38bcd450ac88a2ff47dc5fc38c
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-23 00:47:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,8 @@ accountHttp.getAccountInfo(address)
     for (let m of accountInfo.mosaics) {
          //console.log("m=");
        (async() => {  
-         const mosaicNamesA = await nsRepo.getMosaicsNames([new symbol.MosaicId(m.id.id.toHex())]).toPromise();  
+         const mosaicNamesA = await nsRepo.getMosaicsNames([new symbol.MosaicId(m.id.id.toHex())]).toPromise();
+         console.log(mosaicNamesA);   
          console.log(`${m.id.id.toHex()} : ${[mosaicNamesA][0][0].names[0].name}`);    //モザイクID と　ネームスペース
         })(); // async()    
 
