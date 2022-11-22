388a49e1a2402f338335fa208430beef2078a9e2
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-22 15:04:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ accountHttp.getAccountInfo(address)
   .then((accountInfo) => {
     for (let m of accountInfo.mosaics) {
          console.log("m=");
-         console.log(m);
+         console.log(m.id.id.toHex());
       if (m.id.id.toHex() === XYM_ID) {
         const dom_xym = document.getElementById('wallet-xym')
         dom_xym.innerText = `Balance : ${(parseInt(m.amount.toHex(), 16)/ Math.pow(10, 6)).toLocaleString(undefined, { maximumFractionDigits: 6 })}`
