c452c04f4de0a7d0953943c7d997590db9c27f3b
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-24 20:48:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -457,7 +457,7 @@ function handleSSS() {
      ],
      symbol.PlainMessage.create(message),
      NET_TYPE,
-     symbol.UInt64.fromUint(100000)
+     symbol.UInt64.fromUint(30000)
     )
       
    window.SSS.setTransaction(tx);               // SSSにトランザクションを登録        
