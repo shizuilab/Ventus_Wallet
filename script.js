9690229c06e18a4d5addd0f9491163656c1e4f48
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-25 14:01:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -469,3 +469,32 @@ function handleSSS() {
  })(); // async()  
     
 }
+
+
+// 未承認状態の時にpopup する
+// ポップアップのセッティング処理
+function popupSetting(){
+  let popup = document.getElementById('popup');
+  if(!popup) return;
+
+  let bgBlack = document.getElementById('bg-black');
+  let closeBtn = document.getElementById('close-btn');
+  let showBtn = document.getElementById('show-btn');
+
+  // ポップアップ
+  popUp(bgBlack);
+  popUp(closeBtn);
+  popUp(showBtn);
+
+  // ポップアップ処理
+  function popUp(elem){
+    if(!elem) return;
+  
+    elem.addEventListener('click', function(){
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+
+// ポップアップのセッティング
+popupSetting();
