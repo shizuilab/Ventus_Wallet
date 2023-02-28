b9ef0d403507bd3611eface2ba22b6103721ef8b
mikun <45915246+mikunNEM@users.noreply.github.com>
2023-02-28 12:33:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dom_version = document.getElementById('version');
-dom_version.innerText = 'v1.0.8　|　Powered by SYMBOL';
+dom_version.innerText = 'v1.0.9　|　Powered by SYMBOL';
 
 const sym = require('/node_modules/symbol-sdk');
 const op  = require("/node_modules/rxjs/operators");
